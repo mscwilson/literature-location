caeb8783037d4efc12be77af8a0236c3ca61f68d
Miranda Wilson <mirandascwilson@gmail.com>
2021-05-03 14:48:46 +0100

    Change text colour

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,16 +5,22 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={style.container}>
-      <Text>Welcome to the app</Text>
-      <Button title="go to results" onPress={() => navigation.navigate('Results')}>
-        A button
-      </Button>
+      <View style={{ padding: 50 }}>
+        <Text style={{ color: '#FFA62B', fontSize: 30, marginBottom: 40 }}>Welcome to the app</Text>
+        <Button
+          color="#7D82B8"
+          title="Go to results"
+          onPress={() => navigation.navigate('Results')}>
+          A button
+        </Button>
+      </View>
     </View>
   );
 };
 
 const style = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#DFEFCA',
     alignItems: 'center',
   },
--- src/screens/ResultsScreen.js
+++ src/screens/ResultsScreen.js
@@ -4,13 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 const ResultsScreen = () => {
   return (
     <View style={style.container}>
-      <Text>Results screen</Text>
+      <View style={{ padding: 50 }}>
+        <Text style={{ color: '#AFC2D5', fontSize: 30 }}>This is the results screen</Text>
+      </View>
     </View>
   );
 };
 
 const style = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '#DFEFCA',
     alignItems: 'center',
   },
